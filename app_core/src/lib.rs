7ad1f7517d578fdcab5c7dd24bf0137a09b59262
TiaTaiT <texview@gmx.com>
2026-05-17 16:48:29 +0300

    Add comment

--- app_core/src/lib.rs
+++ app_core/src/lib.rs
@@ -1,4 +1,9 @@
 // File: /app_core/src/lib.rs
+
+// This file only contains the public API of the app_core crate.
+//It re-exports modules and items that are meant to be used by other crates, such as app_firmware.
+// The internal implementation details of the modules are hidden from external users.
+// Don't put any implementation code here; instead, place it in the respective modules (e.g., adc_converter.rs).
 #![cfg_attr(not(test), no_std)]
 
 pub mod hardware_traits;
